b534d802af42cf96c398b6f85c56717b350d8713
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-05-26 14:14:36 +0600

    admin penal added

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 //bed273fce0e18a1a1eca3492a3133d5907a2483f3447948c85217c5794b1f400958f7c4e4a94d9525f0bfa61978896c8774ffd49598482e7a6afe42e4bf52d09
 
 // middleware 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+}));
 app.use(express.json());
 
 
@@ -164,6 +168,41 @@ async function run() {
         const result = await menuCollections.find().toArray();
         res.send(result);
     })
+    app.post('/menu',verifyToken,verifyAdmin, async(req, res)=>{
+      const item = req.body;
+      const result = await menuCollections.insertOne(item);
+      res.send(result);
+    })
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollections.deleteOne(query);
+      res.send(result)
+    })
+    app.get('/menu/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollections.findOne(query);
+      res.send(result);
+    })
+    app.patch('/menu/:id', async(req, res) =>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+      const result = await menuCollections.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+
     app.get('/reviews', async(req, res)=>{
         const result = await reviewsCollections.find().toArray();
         res.send(result);
